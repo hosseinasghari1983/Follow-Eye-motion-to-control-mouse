19e3492d70766e4f2b88b9c33ca1dac36b51347d
Andres Lazo <alazoher@lion.lmu.edu>
2017-04-10 21:18:14 -0700

    Fix columns

--- locktest/patchandmovemouse.m
+++ locktest/patchandmovemouse.m
@@ -21,7 +21,7 @@ function BAS=patchandmovemouse(row,col,WIDTH,HEIGHT,BAS,DIVrow,DIVcol)
 
 
 rowB=ceil(row/HEIGHT*DIVrow);
-colB=floor(col/WIDTH*DIVcol);
+colB=ceil(col/WIDTH*DIVcol);
 BAS(rowB,colB)=BAS(rowB,colB)+1;
 AX=get(0,'screensize');
 
@@ -40,7 +40,7 @@ if BAS(rowB,colB)>4
     colB=DIVcol-colB;
     
     yP = [(rowB-1)*HEIGHT/DIVrow+1 (rowB)*HEIGHT/DIVrow+1 (rowB)*HEIGHT/DIVrow+1 (rowB-1)*HEIGHT/DIVrow+1 ];%[xLinePos1 (width+1) (width+1) xLinePos1];
-    xP = [(colB-1)*WIDTH/DIVcol+1 (colB-1)*WIDTH/DIVcol+1 (colB)*WIDTH/DIVcol+1 (colB)*WIDTH/DIVcol+1];
+    xP = [(colB)*WIDTH/DIVcol+1 (colB)*WIDTH/DIVcol+1 (colB + 1)*WIDTH/DIVcol+1 (colB + 1)*WIDTH/DIVcol+1];
     patch(xP, yP, 'r');
     
     %pos = [pos(1)+20,pos(2)-20];
